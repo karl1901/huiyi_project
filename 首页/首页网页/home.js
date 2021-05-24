02ad0eea5a15126d3eb404f1b280b8bdc4150402
karl <2371265527@qq.com>
2021-05-24 12:54:21 +0800

    回忆项目后台管理vue项目提交

--- 首页/首页网页/home.js
+++ 首页/首页网页/home.js
@@ -35,21 +35,19 @@ win.scroll(function (event) {
 });
 
 // 回到顶部动画
-$(function () {
-  $(window).scroll(function () {
-    //只要窗口滚动,就触发下面代码
-    var scrollt = document.documentElement.scrollTop + document.body.scrollTop; //获取滚动后的高度
-    if (scrollt > 200) {
-      //判断滚动后高度超过200px,就显示
-      $('#top').fadeIn(400); //淡出
-    } else {
-      $('#top').stop().fadeOut(400); //如果返回或者没有超过,就淡入.必须加上stop()停止之前动画,否则会出现闪动
-    }
-  });
-  $('#top').click(function () {
-    //当点击标签的时候,使用animate在200毫秒的时间内,滚到顶部
-    $('html,body').animate({ scrollTop: '0px' }, 200);
-  });
-});
-
-
+// $(function () {
+//   $(window).scroll(function () {
+//     //只要窗口滚动,就触发下面代码
+//     var scrollt = document.documentElement.scrollTop + document.body.scrollTop; //获取滚动后的高度
+//     if (scrollt > 200) {
+//       //判断滚动后高度超过200px,就显示
+//       $('#top').fadeIn(400); //淡出
+//     } else {
+//       $('#top').stop().fadeOut(400); //如果返回或者没有超过,就淡入.必须加上stop()停止之前动画,否则会出现闪动
+//     }
+//   });
+//   $('#top').click(function () {
+//     //当点击标签的时候,使用animate在200毫秒的时间内,滚到顶部
+//     $('html,body').animate({ scrollTop: '0px' }, 200);
+//   });
+// });
